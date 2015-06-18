5f3a513a4a40c8a9c26408b68fb6f583f2f014ac
Joe Leinsdorf <leinsdorf@gmail.com>
2015-06-17 23:22:43 -0400

    crimson prod db functional

--- models/index.js
+++ models/index.js
@@ -7,9 +7,9 @@ var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || "development";
 var config    = require(__dirname + '/../config/config.json')[env];
 
-if (process.env.HEROKU_POSTGRESQL_DATABASE_URL) {
+if (process.env.HEROKU_POSTGRESQL_CRIMSON_URL) {
     // the application is executed on Heroku ... use the postgres database
-    sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_DATABASE_URL, {
+    sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_CRIMSON_URL, {
       dialect:  'postgres',
       protocol: 'postgres',
       logging:  true //false
