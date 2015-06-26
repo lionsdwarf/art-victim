f8195cc7e2f54de3d23ac441747851c4bc02ce71
Joe Leinsdorf <leinsdorf@gmail.com>
2015-06-25 20:57:25 -0400

    tabbing funcitonal + css

--- public/js/views/library_view.js
+++ public/js/views/library_view.js
@@ -1,13 +1,21 @@
 App.Views.Library = Backbone.View.extend({
   className: 'library',
   initialize: function() {
-    this.template = Handlebars.compile($('#library-template').html());
+    this.libraryTabTemplate = Handlebars.compile($('#library-tab-template').html());
+    this.libraryTemplate = Handlebars.compile($('#library-template').html());
+    this.el = $(this.libraryTabTemplate(this.model.toJSON()));
+    this.renderLibraryTabs();
+  },
+
+  renderLibraryTabs: function() {
+    var libTabTemplate = this.libraryTabTemplate(this.model.toJSON());
+    this.$el.html(libTabTemplate);
     this.render();
   },
 
   render: function() {
-    var libraryTemplate = this.template(this.model.toJSON());
-    this.$el.html(libraryTemplate);
+    var libraryTemplate = this.libraryTemplate(this.model.toJSON());
+    $('#tabs').append(libraryTemplate);
     //pause to render templates which contain els for graphicsCollectionViews
     setTimeout(function() { this.renderLibraryGraphicsCollection() }.bind(this), 400);
   },
